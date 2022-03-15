dcad64f3a739c3c482469de3834f8e23a4473872
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2022-03-15 02:12:28 -0400

    Add files via upload
    HashMap to add items to it, we use the put() method

---
+++ Day1HashMap.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.HashMap;
+
+public class Day1HashMap {
+
+	public static void main(String[] args) {
+		
+		HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+	    // Add keys and values (Country, City)
+	    capitalCities.put("England", "London");
+	    capitalCities.put("Germany", "Berlin");
+	    capitalCities.put("Norway", "Oslo");
+	    capitalCities.put("USA", "Washington DC");
+	    System.out.println(capitalCities);
+	}
+
+}
