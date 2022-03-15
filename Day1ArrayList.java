1ff1e11a83d39f2355b66b3e1eccc31ed0c13eb4
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2022-03-15 01:25:44 -0400

    Add files via upload
    ArrayList with the for-each loop

---
+++ Day1ArrayList.java
@@ -0,0 +1,19 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class Day1ArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<String> cars = new ArrayList<String>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+	    for (String i : cars) {
+	      System.out.println(i);
+	    }
+
+	}
+
+}
