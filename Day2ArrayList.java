f328abb1b6c4f48b4b5c2ed7872455462e940da2
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2022-03-15 02:20:07 -0400

    Add files via upload
    ArrayList to store numbers

---
+++ Day2ArrayList.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.ArrayList;
+
+public class Day2ArrayList {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+	    myNumbers.add(10);
+	    myNumbers.add(15);
+	    myNumbers.add(20);
+	    myNumbers.add(25);
+	    for (int i : myNumbers) {
+	      System.out.println(i);
+	    }
+	}
+
+}
