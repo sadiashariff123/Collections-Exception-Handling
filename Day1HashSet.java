1a2cc965ef706bdb94ddd5b4f97ccadc68ec4e98
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2022-03-15 02:27:41 -0400

    Add files via upload
    HashSet with add() method.

---
+++ Day1HashSet.java
@@ -0,0 +1,20 @@
+package Collections;
+
+import java.util.HashSet;
+
+public class Day1HashSet {
+
+	public static void main(String[] args) {
+
+
+		 HashSet<String> cars = new HashSet<String>();
+		    cars.add("Volvo");
+		    cars.add("BMW");
+		    cars.add("Ford");
+		    cars.add("BMW");
+		    cars.add("Mazda");
+		    System.out.println(cars);
+
+	}
+
+}
