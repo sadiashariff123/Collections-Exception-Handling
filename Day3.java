6ee1a48b779b4f1570f4ef7fee717d8f4b280201
sadiashariff123 <95458931+sadiashariff123@users.noreply.github.com>
2022-03-17 02:01:36 -0400

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,21 @@
+package Exceptions;
+
+public class Day3 {
+
+	public static void main(String[] args) {
+		
+		
+
+try
+ {
+  int[] myNumbers = {1, 2, 3};
+  System.out.println(myNumbers[10]);
+} 
+catch
+ (Exception e) {
+  System.out.println("Something went wrong.");
+}
+
+	}
+
+}
